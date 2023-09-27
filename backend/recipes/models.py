79a379429caa5f14e13ea1b67a311ec16b642d17
Vsetsko <vsetsko2003@gmail.com>
2023-09-27 19:18:54 +0300

    Пятое ревью

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -6,7 +6,7 @@ from django.db.models import (CASCADE, CharField, DateTimeField, ForeignKey,
                               UniqueConstraint)
 
 from users.models import User
-from validators import validate_hex
+from .validators import validate_hex
 
 
 class Ingredient(Model):
